a8c2703b236a18f844f83e24770b7c80115b59b5
RyanPark2323 <133062764+RyanPark2323@users.noreply.github.com>
2026-02-16 16:34:46 -0700

    Forgot to add main, updated code formatting

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
 @app.route('/')
 
 def home():
     return "<h1>This is a simple Spider Trap Implementation:</h1><p>Welcome to the spider trap! This page is designed to trap unethical web crawlers and bots, but it will not harm users or ethical web crawlers and bots</p>"
+
+def spider_trap():
+    # To Do: Implement the spider trap logic here
+    pass
+
+if __name__ == '__main__':
+    app.run(debug=True)
