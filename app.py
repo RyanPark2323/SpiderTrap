c74695a10e30820f30e98b430bbfff6622912f31
RyanPark2323 <133062764+RyanPark2323@users.noreply.github.com>
2026-02-16 16:32:56 -0700

    Set up a base flask homepage

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+    return "<h1>This is a simple Spider Trap Implementation:</h1><p>Welcome to the spider trap! This page is designed to trap unethical web crawlers and bots, but it will not harm users or ethical web crawlers and bots</p>"
