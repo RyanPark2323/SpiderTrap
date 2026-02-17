8addb985ece2eca5c921373d2039f18f7b27efc3
RyanPark2323 <133062764+RyanPark2323@users.noreply.github.com>
2026-02-16 17:13:59 -0700

    Add default path and pathID=None argument for fix

--- app.py
+++ app.py
@@ -8,8 +8,9 @@ app = Flask(__name__)
 def home():
     return "<h1>This is a simple Spider Trap Implementation:</h1><p>Welcome to the spider trap! This page is designed to trap unethical web crawlers and bots, but it will not harm users or ethical web crawlers and bots</p>"
 
+@app.route('/system-logs-archive/')
 @app.route('/system-logs-archive/<path_id>')
-def spider_trap():
+def spider_trap(path_id=None):
     new_paths = [str(uuid.uuid4()) for _ in range(10)]
     return render_template('spider_trap.html', paths=new_paths)
 
