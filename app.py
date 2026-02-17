1d5b3762aa13f8b93be2dde3c6f7fe03481e2280
RyanPark2323 <133062764+RyanPark2323@users.noreply.github.com>
2026-02-16 17:03:18 -0700

    Implement spider_trap method

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, send_from_directory
+import uuid
+from flask import Flask, send_from_directory, render_template
 import os
 
 app = Flask(__name__)
@@ -9,8 +10,8 @@ def home():
     return "<h1>This is a simple Spider Trap Implementation:</h1><p>Welcome to the spider trap! This page is designed to trap unethical web crawlers and bots, but it will not harm users or ethical web crawlers and bots</p>"
 
 def spider_trap():
-    # To Do: Implement the spider trap logic here
-    pass
+    new_paths = [str(uuid.uuid4()) for _ in range(10)]
+    return render_template('spider_trap.html', paths=new_paths)
 
 @app.route('/robots.txt')
 def static_from_root():
